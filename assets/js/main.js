abfc04e3bc471107ec64a2eadd9c88b96a5b6720
didar3ar <didar3ar@gmail.com>
2017-11-15 08:03:41 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       $(".logo-carousel").owlCarousel({
+            items: 6,
+            margin:30,
+            loop:true,
+            dots:false,
+            nav:false,
+            autoplay: true,
+            smartSpeed: 800,
+       });
+       $(".case-studies-carousel").owlCarousel({
+            items: 3,
+            margin:30,
+            loop:true,
+            dots:true,
+            nav:false,
+            autoplay: true,
+            smartSpeed: 800,
+       });
+
+
+       $('.parallax-bg').parallax("50%", -0.5);
+
+
+
+
+
+
+       $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            loop:true,
+            dots:true,
+            nav:false,
+            autoplay: true,
+            smartSpeed: 800,
+       });
+
+
+    });
+
+    jQuery(window).load(function(){
+        
+    });
+
+
+}(jQuery));	
